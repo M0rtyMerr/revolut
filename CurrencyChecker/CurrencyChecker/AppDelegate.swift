230c4c109e45a50f1ebf6e052733cefa78a3cabc
Anton Nazarov <nazarof.anton@gmail.com>
2018-07-23 20:31:55 +0300

    Initial setup

---
+++ CurrencyChecker/CurrencyChecker/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  CurrencyChecker
+//
+//  Created by Антон Назаров on 23/07/2018.
+//  Copyright © 2018 Антон Назаров. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
+
